a13456a563be126733e53242dd0fbffcbabe99a8
hannahwn <hannahwanjikunyongo@gmail.com>
2026-03-25 22:27:50 +0100

    checked time works

--- task-1/Time.js
+++ task-1/Time.js
@@ -1,3 +1,43 @@
 export class Time {
   // Your code here
+
+  #secondsFromMidnight;
+
+  constructor(hours, minutes, seconds) {
+    const total = hours * 3600 + minutes * 60 + seconds;
+
+    this.#secondsFromMidnight = total;
+  }
+
+  getHours() {
+    return Math.floor(this.#secondsFromMidnight / 3600);
+  }
+
+  getMinutes() {
+    return Math.floor((this.#secondsFromMidnight % 3600) / 60);
+  }
+
+  getSeconds() {
+    return this.#secondsFromMidnight % 60;
+  }
+
+  addSeconds(seconds) {
+    this.#secondsFromMidnight =
+      (((this.#secondsFromMidnight + seconds) % 86400) + 86400) % 86400;
+  }
+
+  addMinutes(minutes) {
+    this.addSeconds(minutes * 60);
+  }
+
+  addHours(hours) {
+    this.addSeconds(hours * 3600);
+  }
+
+  toString() {
+    const h = String(this.getHours()).padStart(2, "0");
+    const m = String(this.getMinutes()).padStart(2, "0");
+    const s = String(this.getSeconds()).padStart(2, "0");
+    return `${h}:${m}:${s}`;
+  }
 }
--- task-1/index.js
+++ task-1/index.js
@@ -1,4 +1,17 @@
-import { Time } from './Time.js';
+import { Time } from "./Time.js";
 
 const time = new Time(13, 37, 0);
 console.log(time.toString()); // Output: "13:37:00"
+
+time.getHours();
+time.getMinutes();
+time.getSeconds();
+
+time.addHours(40);
+console.log("After +40 hours:", time.toString()); //output 06:17:00
+
+time.addMinutes(40);
+console.log("After +40 mins:", time.toString()); //output 14:17:00
+
+time.addSeconds(30);
+console.log("After +30 seconds:", time.toString()); // output 06:17:30
